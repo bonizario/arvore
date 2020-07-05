b1f02ae13f699df4fad1843af957ffffdec464f0
Bonizario <gabrielbonizario98@gmail.com>
2020-07-05 03:36:47 -0300

    feat: add homepage with bootstrap and responsive gallery

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const routes = require('./routes');
 
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
-server.use(routes);
 
 server.set('view engine', 'njk');
 
-nunjucks.configure('src/app/views', {
+nunjucks.configure('src/views', {
   express: server,
   autoescape: false,
   noCache: true,
+});
+
+server.get('/livros/1', (req, res) => {
+  return res.render('book.njk');
+});
+
+server.get('/', (req, res) => {
+  return res.render('home.njk');
 });
 
 const port = 5000;
